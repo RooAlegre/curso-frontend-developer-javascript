98d01f4a425b7b7da6b0726b11ff89953f5a1ec7
Rocío <rocio29_alegre@hotmail.com>
2023-07-17 17:47:33 -0300

    Se me desmadró todo y lo solucioné

--- main.js
+++ main.js
@@ -1,98 +1,100 @@
-const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
-const menuBurger = document.querySelector(".menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
-const aside = document.querySelector(".shopping-cart-detail");
-const cardsContainer = document.querySelector('.cards-container');
+const menuEmail=document.querySelector('.navbar-email');
+const menuHamIcon=document.querySelector('.menu');
+const menuCarritoIcon=document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon=document.querySelector('.product-detail-close')
+const desktopMenu=document.querySelector('.desktop-menu');
+const mobileMenu=document.querySelector('.mobile-menu');
+const shoppingCartContainer=document.querySelector('#shoppingCartContainer');
+const productDetailContainer=document.querySelector('#productDetail');
+const cardsContainer=document.querySelector('.cards-container');
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
-menuBurger.addEventListener("click", toggleMobileMenu);
-menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click',closeProductDetailAside);
 
-
-function toggleDesktopMenu (){
- const isAsideClosed=aside.classList.contains("inactive");
-    if(!isAsideClosed){
-     aside.classList.add("inactive");
+function toggleDesktopMenu(){
+    const isAsideClosed=shoppingCartContainer.classList.contains('inactive');
+    if(!isAsideClosed)
+    {shoppingCartContainer.classList.add('inactive');
     }
-
-    desktopMenu.classList.toggle("inactive");
+   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-   const isAsideClosed=aside.classList.contains("inactive");
-      if(!isAsideClosed){
-        aside.classList.add("inactive");
+    const isAsideClosed=shoppingCartContainer.classList.contains('inactive');
+    if(!isAsideClosed){
+        shoppingCartContainer.classList.add('inactive');
     }
-
-  mobileMenu.classList.toggle("inactive");
+    closeProductDetailAside();mobileMenu.classList.toggle('inactive');
 }
 
 function toggleCarritoAside(){
-    const isMobileMenuClosed=mobileMenu.classList.contains("inactive");
-       if(!isMobileMenuClosed){
-        mobileMenu.classList.add("inactive");
+    const isMobileMenuClosed=mobileMenu.classList.contains('inactive');
+    if(!isMobileMenuClosed){mobileMenu.classList.add('inactive');
     }
-
-    aside.classList.toggle("inactive");
+    const isProductDetailClosed=productDetailContainer.classList.contains('inactive');
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
+    }
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
-const productList = [];
-productList.push({
-    name: "bike",
-    price: 120,
-    image: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-})
+function openProductDetailAside(){
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+}
+
+function closeProductDetailAside(){
+    productDetailContainer.classList.add('inactive');
+}
+    
+const productList=[];
 
 productList.push({
-    name: "pantalla",
-    price: 220,
-    image: "https://s3.abcstatics.com/media/favorito/2021/11/29/mejores-monitores-PC-oferta-U72777153102iNy-620x349@abc.jpg"
-})
+    name:'Bike',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
 
 productList.push({
-    name: "computadora",
-    price: 620,
-    image: "https://www.tuexperto.com/wp-content/uploads/2022/02/10-fondos-de-pantalla-para-pc-con-resolucion-4k-1.jpg.webp"
-})
+    name:'Pantalla',
+    price:220,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
 
-function renderProducts(arr) {
-    for (product of arr) {
-      const productCard = document.createElement('div');
-      productCard.classList.add('product-card');
-    
-      // product= {name, price, image} -> product.image
-      const productImg = document.createElement('img');
-      productImg.setAttribute('src', product.image);
-    
-      const productInfo = document.createElement('div');
-      productInfo.classList.add('product-info');
-    
-      const productInfoDiv = document.createElement('div');
-    
-      const productPrice = document.createElement('p');
-      productPrice.innerText = '$' + product.price;
-      const productName = document.createElement('p');
-      productName.innerText = product.name;
-    
-      productInfoDiv.appendChild(productPrice);
-      productInfoDiv.appendChild(productName);
-    
-      const productInfoFigure = document.createElement('figure');
-      const productImgCart = document.createElement('img');
-      productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
-    
-      productInfoFigure.appendChild(productImgCart);
-    
-      productInfo.appendChild(productInfoDiv);
-      productInfo.appendChild(productInfoFigure);
-    
-      productCard.appendChild(productImg);
-      productCard.appendChild(productInfo);
-    
-      cardsContainer.appendChild(productCard);
+productList.push({
+    name:'Compu',
+    price:620,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+function renderProducts(arr){
+    for(product of arr){
+        const productCard=document.createElement('div');
+        productCard.classList.add('product-card');
+        const productImg=document.createElement('img');
+        productImg.setAttribute('src',product.image);
+        productImg.addEventListener('click',openProductDetailAside);
+        const productInfo=document.createElement('div');
+        productInfo.classList.add('product-info');
+        const productInfoDiv=document.createElement('div');
+        const productPrice=document.createElement('p');
+        productPrice.innerText='$'+product.price;
+        const productName=document.createElement('p');
+        productName.innerText=product.name;productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+        const productInfoFigure=document.createElement('figure');
+        const productImgCart=document.createElement('img');
+        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+        productInfoFigure.appendChild(productImgCart);
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+        cardsContainer.appendChild(productCard);
     }
 }
-  
-renderProducts(productList);
+
+    renderProducts(productList);
